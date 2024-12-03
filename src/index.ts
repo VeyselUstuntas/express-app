2f8f7b16dfb545d3bba823ab2413738306d4b0ef
veyselustuntas <veyselustuntas@hotmail.com>
2024-12-03 14:26:03 +0300

    bs kütüphanesi eklendi

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@ import express, { Request, Response } from "express";
 import path from "path";
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../views"));
+console.log(path.join(__dirname))
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.static("node_modules"));
 
 const data = [
-    { id: 1, name: "iphone 14", price: 30000, isActive: false },
-    { id: 2, name: "iphone 15", price: 40000, isActive: true },
-    { id: 3, name: "iphone 16", price: 50000, isActive: true }
+    { id: 1, name: "iphone 14", price: 30000, isActive: false, isHome: false, image: "1.jpg" },
+    { id: 2, name: "iphone 15", price: 40000, isActive: true, isHome: true, image: "2.jpg" },
+    { id: 3, name: "iphone 16", price: 50000, isActive: true, isHome: false, image: "3.jpg" }
 ];
 
 app.get("/product/:id", (req: Request, res: Response) => {
@@ -23,7 +26,7 @@ app.use("/products", (req: Request, res: Response) => {
 });
 
 app.use("/", (req: Request, res: Response) => {
-    res.render('index');
+    res.render('index',{ data: data });
 
 });
 
