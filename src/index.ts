a54f4a5b1d6f040c99752c5f177624eda8e947fb
veyselustuntas <veyselustuntas@hotmail.com>
2024-12-03 12:06:18 +0300

    feat: urun detay sayfası tamamlandı

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,14 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 const data = [
-    { id: 1, name: "iphone 14", price: 30000, isActive: true },
+    { id: 1, name: "iphone 14", price: 30000, isActive: false },
     { id: 2, name: "iphone 15", price: 40000, isActive: true },
     { id: 3, name: "iphone 16", price: 50000, isActive: true }
 ];
 
 app.get("/product/:id", (req: Request, res: Response) => {
-    res.render("product-details");
+    const product = data.find(u => u.id === Number(req.params.id));
+    res.render("product-details", product);
 
 });
 
