760f545113c451a67eafa9a7f472637d4abf3e60
veyselustuntas <veyselustuntas@hotmail.com>
2024-12-03 10:51:45 +0300

    feat: route yapısı eklendi

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import express, { Request, Response } from "express";
 const app = express();
 
+app.use("/products/:id", (req: Request, res: Response) => {
+    res.send("Product Details " + req.params.id);
+
+});
+
+app.use("/products", (req: Request, res: Response) => {
+    res.send("Product Page");      
+
+});
+
 app.use("/", (req: Request, res: Response) => {
     res.send("Home Page");      
 
@@ -9,3 +19,4 @@ app.use("/", (req: Request, res: Response) => {
 app.listen(3000, () => {
     console.log("node server listening on port 3000");
 });
+  
