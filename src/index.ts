e1847ada1ced11c75dcd6ddb3490b49be6ff2473
veyselustuntas <veyselustuntas@hotmail.com>
2024-12-03 11:27:32 +0300

    feat: dinamik veri ile calisma

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,19 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const data = [
+    { id: 1, name: "iphone 14", price: 30000 },
+    { id: 2, name: "iphone 15", price: 40000 },
+    { id: 3, name: "iphone 16", price: 50000 }
+];
+
 app.get("/products/:id", (req: Request, res: Response) => {
     res.render("product-details");
 
 });
 
 app.use("/products", (req: Request, res: Response) => {
-    res.render("product");
+    res.render("products",{data:data});
 
 });
 
